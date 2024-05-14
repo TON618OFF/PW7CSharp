558281bd2d8d93732ab2aca914f7181ddd923b67
TON618OFF <rusblackgod@gmail.com>
2024-05-14 18:39:55 +0300

    Добавьте файлы проекта.

---
+++ Models/MessengerClient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice_7.Models
+{
+    public class MessengerClient
+    {
+        private Socket _socket;
+        private string _ipAddress;
+        private int _port;
+
+        public MessengerClient(string ipAddress, int port)
+        {
+            _ipAddress = ipAddress;
+            _port = port;
+        }
+
+        public async Task ConnectAsync()
+        {
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            await _socket.ConnectAsync(IPAddress.Parse(_ipAddress), _port);
+        }
+
+        public async Task SendMessageAsync(string message)
+        {
+            var noliki = Encoding.UTF8.GetBytes(message);
+            await _socket.SendAsync(noliki);
+        }
+
+        public async Task<string> ReceiveMessageAsync()
+        {
+            byte[] noliki = new byte[1024];
+            await _socket.ReceiveAsync(noliki);
+            return Encoding.UTF8.GetString(noliki);
+        }
+
+        public void Close()
+        {
+            _socket.Close();
+        }
+    }
+}
---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using practice_7.Models;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading.Tasks;
+
+namespace practice_7
+{
+
+    public partial class MainWindow : Window
+    {
+        private MessengerClient _client;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _client = new MessengerClient("26.17.59.203", 5000);
+            _client.MessageReceived += OnMessageReceived;
+        }
+
+        private async void OnMessageReceived(object sender, string message)
+        {
+            pole_message.Items.Add(message);
+        }
+
+        private async void OnSendButtonClick(object sender, RoutedEventArgs e)
+        {
+            string message = your_message.Text;
+            await _client.SendMessageAsync(message);
+            your_message.Clear();
+        }
+
+        private void OnWindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            _client.Close();
+        }
+    }
+}
